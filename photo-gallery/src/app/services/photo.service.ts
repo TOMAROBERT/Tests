931c7de61786a9d290d3454793beb0a7a37735ba
tomarobert <roberttoma97@gmail.com>
2020-11-09 08:05:51 +0200

    first ionic app

---
+++ photo-gallery/src/app/services/photo.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
+         CameraPhoto, CameraSource } from '@capacitor/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  constructor() { }
+}
